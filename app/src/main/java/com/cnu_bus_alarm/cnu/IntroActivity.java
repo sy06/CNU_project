b0544f6c5b5d72cb9f74ccc0b5034689e9cbd98f
sy06 <gmlakd8944@cnu.ac.kr>
2018-01-06 01:36:26 +0900

    intro 1st implement

---
+++ app/src/main/java/com/cnu_bus_alarm/cnu/IntroActivity.java
@@ -0,0 +1,40 @@
+package com.cnu_bus_alarm.cnu;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+/**
+ * Created by 진수연 on 2018-01-06.
+ */
+
+public class IntroActivity extends Activity {
+    Handler handler = new Handler();
+    Runnable r = new Runnable(){
+        @Override
+        public void run(){
+            Intent intent = new Intent (getApplicationContext(), MainActivity.class);
+            startActivity(intent); //다음화면으로 넘어감
+            finish();
+        }
+    };
+
+    @Override
+    protected  void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.intro_layout); //xml , java 소스 연결
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume(); //다시 화면에 들어오면 예약 걸어줌
+        handler.postDelayed(r, 4000); //4초 뒤에 runnable 객체 r 실행
+    }
+
+    @Override
+    protected  void onPause(){
+        super.onPause(); //화면을 벗어나면, 예약한 작업 취소
+        handler.removeCallbacks(r); //예약 취소
+    }
+}
