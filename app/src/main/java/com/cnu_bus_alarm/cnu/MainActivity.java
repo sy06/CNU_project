d0f2a4f09b9698310fa90a46d46f7f6bb95b065c
sy06 <gmlakd8944@cnu.ac.kr>
2018-01-16 18:26:16 +0900

    2nd implement

--- app/src/main/java/com/cnu_bus_alarm/cnu/MainActivity.java
+++ app/src/main/java/com/cnu_bus_alarm/cnu/MainActivity.java
@@ -1,13 +1,21 @@
 package com.cnu_bus_alarm.cnu;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void LocationClicked(View v){
+        Toast.makeText(this,"잠시만 기다려주세요.",Toast.LENGTH_LONG).show();
+        Intent intent = new Intent(getApplicationContext(), LocationSubActivity.class);
+        startActivity(intent);
     }
 }
